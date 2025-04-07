342600068e2b59fb6d1403c3552fe103b35b52f5
Adrian Kafel <adrian.kafel@proton.me>
2025-04-07 19:19:58 +0200

    add grade component

---
+++ src/lib/Grade.svelte
@@ -0,0 +1,20 @@
+<script>
+  const colorVariants = {
+    1: "bg-red-600 hover:bg-red-700",
+    2: "bg-orange-500 hover:bg-orange-600",
+    3: "bg-yellow-300 hover:bg-yellow-400",
+    4: "bg-lime-400 hover:bg-lime-500",
+    5: "bg-green-600 hover:bg-green-700",
+    6: "bg-blue-600 hover:bg-blue-700",
+  };
+
+  let { value } = $props();
+</script>
+
+<div
+  class={`${
+    colorVariants[value]
+  } p-2 rounded-lg size-8 flex justify-center items-center`}
+>
+  <span class="font-semibold">{value}</span>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
+<script>
+  import Grade from "$lib/Grade.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
-  the documentation
-</p>
+<div class="flex justify-center items-center">
+  <Grade value={1} />
+  <Grade value={2} />
+  <Grade value={3} />
+  <Grade value={4} />
+  <Grade value={5} />
+  <Grade value={6} />
+</div>
